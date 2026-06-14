49da6e7fbb2273d0cb61f43447ad5b7b0b7354fd
Zulfa Fatah Akbar Ahmad <63113235+Zulfaabam@users.noreply.github.com>
2026-06-14 20:51:12 +0700

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public int MaxProfit(int[] prices) {
+        int l = 0;
+        int r = l + 1;
+
+        int profit = 0;
+
+        while (r < prices.Length) {
+            if (prices[r] > prices[l]) {
+                profit = Math.Max(profit, prices[r] - prices[l]);
+            } else {
+                l = r;
+            }
+
+            r += 1;
+        }
+
+        return profit;
+    }
+}
